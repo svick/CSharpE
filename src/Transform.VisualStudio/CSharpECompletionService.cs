7fe9a03b64b0d16aa4d81015f15cad961c6e8626
Petr Onderka <gsvick@gmail.com>
2018-10-06 01:00:43 +0200

    Also delegate virtual methods, to get the same behavior as C#

--- src/Transform.VisualStudio/CSharpECompletionService.cs
+++ src/Transform.VisualStudio/CSharpECompletionService.cs
@@ -3,6 +3,7 @@ using Microsoft.CodeAnalysis.Completion;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Options;
+using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.Composition;
 using System;
 using System.Collections.Immutable;
@@ -15,7 +16,7 @@ using static Microsoft.CodeAnalysis.LanguageNames;
 namespace CSharpE.Transform.VisualStudio
 {
     [ExportLanguageServiceFactory(typeof(CompletionService), CSharp, ServiceLayer.Host)]
-    internal class CSharpECompletionServiceFactory : ILanguageServiceFactory
+    internal sealed class CSharpECompletionServiceFactory : ILanguageServiceFactory
     {
         private readonly ExportProvider exportProvider;
 
@@ -26,7 +27,7 @@ namespace CSharpE.Transform.VisualStudio
             new CSharpECompletionService(exportProvider, languageServices);
     }
 
-    class CSharpECompletionService : CompletionService
+    internal sealed class CSharpECompletionService : CompletionService
     {
         private readonly CompletionService cSharpCompletionService;
 
@@ -46,13 +47,38 @@ namespace CSharpE.Transform.VisualStudio
 
         public override string Language => CSharp;
 
-        public override async Task<CompletionList> GetCompletionsAsync(Document document, int caretPosition, CompletionTrigger trigger = default,
-            ImmutableHashSet<string> roles = null, OptionSet options = null, CancellationToken cancellationToken = default)
+        public override async Task<CompletionList> GetCompletionsAsync(
+            Document document, int caretPosition, CompletionTrigger trigger = default,
+            ImmutableHashSet<string> roles = null, OptionSet options = null,
+            CancellationToken cancellationToken = default)
         {
             var completionList = await cSharpCompletionService.GetCompletionsAsync(
                 document, caretPosition, trigger, roles, options, cancellationToken);
 
             return completionList.WithItems(completionList.Items.Insert(0, CompletionItem.Create("CSHARPEEE")));
         }
+
+        public override ImmutableArray<CompletionItem> FilterItems(
+            Document document, ImmutableArray<CompletionItem> items, string filterText) =>
+            cSharpCompletionService.FilterItems(document, items, filterText);
+
+        public override Task<CompletionChange> GetChangeAsync(
+            Document document, CompletionItem item, char? commitCharacter = null,
+            CancellationToken cancellationToken = default) =>
+            cSharpCompletionService.GetChangeAsync(document, item, commitCharacter, cancellationToken);
+
+        public override TextSpan GetDefaultCompletionListSpan(SourceText text, int caretPosition) =>
+            cSharpCompletionService.GetDefaultCompletionListSpan(text, caretPosition);
+
+        public override Task<CompletionDescription> GetDescriptionAsync(
+            Document document, CompletionItem item, CancellationToken cancellationToken = default) =>
+            cSharpCompletionService.GetDescriptionAsync(document, item, cancellationToken);
+
+        public override CompletionRules GetRules() => cSharpCompletionService.GetRules();
+
+        public override bool ShouldTriggerCompletion(
+            SourceText text, int caretPosition, CompletionTrigger trigger, ImmutableHashSet<string> roles = null,
+            OptionSet options = null) =>
+            cSharpCompletionService.ShouldTriggerCompletion(text, caretPosition, trigger, roles, options);
     }
 }
