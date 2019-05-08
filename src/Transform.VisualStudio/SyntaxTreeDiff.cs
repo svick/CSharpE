57484adbe57a5b73aa103aa8d3f3e34804e672df
Petr Onderka <gsvick@gmail.com>
2019-05-08 15:03:29 +0200

    Better Adjust for diagnostics

--- src/Transform.VisualStudio/SyntaxTreeDiff.cs
+++ src/Transform.VisualStudio/SyntaxTreeDiff.cs
@@ -9,19 +9,103 @@ using RoslynSyntaxTree = Microsoft.CodeAnalysis.SyntaxTree;
 
 namespace CSharpE.Transform.VisualStudio
 {
-    public class SyntaxTreeDiff
+    public class SyntaxDiff
+    {
+        private readonly SyntaxNode oldNode;
+        private readonly SyntaxNode newNode;
+
+        protected Lazy<IList<TextChange>> Changes { get; }
+
+        protected virtual IList<TextChange> ComputeChanges() => SyntaxDiffer.GetTextChanges(oldNode, newNode);
+
+        public SyntaxDiff(SyntaxNode oldNode, SyntaxNode newNode)
+        {
+            this.oldNode = oldNode;
+            this.newNode = newNode;
+
+            Changes = new Lazy<IList<TextChange>>(ComputeChanges);
+        }
+
+        public TextSpan? Adjust(TextSpan span)
+        {
+            int? start = Adjust(span.Start);
+            if (start == null)
+                return null;
+
+            int? end = Adjust(span.End);
+            if (end == null)
+                return null;
+
+            return TextSpan.FromBounds(start.Value, end.Value);
+        }
+
+        public TextSpan AdjustLoose(TextSpan span)
+        {
+            int start = AdjustLoose(span.Start);
+
+            int end = AdjustLoose(span.End);
+
+            return TextSpan.FromBounds(start, end);
+        }
+
+        public int? Adjust(int position)
+        {
+            int? result = position;
+
+            foreach (var change in Changes.Value)
+            {
+                result = Adjust(position, result, change);
+            }
+
+            return result;
+        }
+
+        private static int? Adjust(int originalPosition, int? position, TextChangeRange change, bool loose = false)
+        {
+            if (position == null)
+                return null;
+
+            var pos = position.Value;
+
+            int diff = change.NewLength - change.Span.Length;
+
+            if (change.Span.End <= originalPosition)
+                return pos + diff;
+
+            if (change.Span.Start > originalPosition)
+                return pos;
+
+            if (loose)
+                return pos - (originalPosition - change.Span.Start);
+
+            return null;
+        }
+
+        public int AdjustLoose(int position)
+        {
+            int? result = position;
+
+            foreach (var change in Changes.Value)
+            {
+                result = Adjust(position, result, change, loose: true);
+            }
+
+            return result.Value;
+        }
+    }
+
+    public class SyntaxTreeDiff : SyntaxDiff
     {
         private readonly RoslynSyntaxTree oldTree;
         private readonly RoslynSyntaxTree newTree;
 
-        private readonly Lazy<IList<TextChange>> changes;
+        protected override IList<TextChange> ComputeChanges() => SyntaxDiffer.GetTextChanges(oldTree, newTree);
 
         public SyntaxTreeDiff(RoslynSyntaxTree oldTree, RoslynSyntaxTree newTree)
+            : base(oldTree.GetRoot(), newTree.GetRoot())
         {
             this.oldTree = oldTree;
             this.newTree = newTree;
-
-            changes = new Lazy<IList<TextChange>>(() => SyntaxDiffer.GetTextChanges(oldTree, newTree));
         }
 
         public Diagnostic Adjust(Diagnostic diagnostic) => diagnostic.WithLocation(AdjustLoose(diagnostic.Location));
@@ -56,82 +140,23 @@ namespace CSharpE.Transform.VisualStudio
             var oldNode = location.SourceTree.GetRoot().FindNode(location.SourceSpan);
             var nodePair = FindNodePair(oldNode);
 
+            TextSpan adjusted;
             if (nodePair != null)
             {
-                // TODO: if location != oldNode.Location, use diff to compute better adjusted location
-                return nodePair.Value.newNode.GetLocation();
-            }
+                var (foundOldNode, foundNewNode) = nodePair.Value;
 
-            var adjusted = AdjustLoose(location.SourceSpan);
+                if (location == foundOldNode.GetLocation())
+                    return foundNewNode.GetLocation();
+
+                var nodeDiff = new SyntaxDiff(foundOldNode, foundNewNode);
+                adjusted = nodeDiff.AdjustLoose(location.SourceSpan);
+            }
+            else
+            {
+                adjusted = AdjustLoose(location.SourceSpan);
+            }
 
             return Location.Create(newTree, adjusted);
-        }
-
-        public TextSpan? Adjust(TextSpan span)
-        {
-            int? start = Adjust(span.Start);
-            if (start == null)
-                return null;
-
-            int? end = Adjust(span.End);
-            if (end == null)
-                return null;
-
-            return TextSpan.FromBounds(start.Value, end.Value);
-        }
-
-        private TextSpan AdjustLoose(TextSpan span)
-        {
-            int start = AdjustLoose(span.Start);
-
-            int end = AdjustLoose(span.End);
-
-            return TextSpan.FromBounds(start, end);
-        }
-
-        public int? Adjust(int position)
-        {
-            int? result = position;
-
-            foreach (var change in changes.Value)
-            {
-                result = Adjust(position, result, change);
-            }
-
-            return result;
-        }
-
-        private static int? Adjust(int originalPosition, int? position, TextChangeRange change, bool loose = false)
-        {
-            if (position == null)
-                return null;
-
-            var pos = position.Value;
-
-            int diff = change.NewLength - change.Span.Length;
-
-            if (change.Span.End <= originalPosition)
-                return pos + diff;
-
-            if (change.Span.Start > originalPosition)
-                return pos;
-
-            if (loose)
-                return pos - (originalPosition - change.Span.Start);
-
-            return null;
-        }
-
-        public int AdjustLoose(int position)
-        {
-            int? result = position;
-
-            foreach (var change in changes.Value)
-            {
-                result = Adjust(position, result, change, loose: true);
-            }
-
-            return result.Value;
         }
     }
 }
