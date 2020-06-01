9e2a8d289dbddd32430bbab90152478891361f69
Petr Onderka <gsvick@gmail.com>
2020-06-01 20:59:25 +0200

    Move patterns into a separate directory

---
+++ src/Syntax/Expressions/Patterns/ConstantPattern.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using CSharpE.Syntax.Internals;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RoslynSyntaxFactory = Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+using Roslyn = Microsoft.CodeAnalysis;
+
+namespace CSharpE.Syntax
+{
+    public sealed class ConstantPattern : Pattern
+    {
+        private ConstantPatternSyntax syntax;
+
+        internal ConstantPattern(ConstantPatternSyntax syntax, SyntaxNode parent)
+            : base(syntax)
+        {
+            this.syntax = syntax;
+            Parent = parent;
+        }
+
+        public ConstantPattern(Expression expression) => Expression = expression;
+
+        private Expression expression;
+        public Expression Expression
+        {
+            get
+            {
+                if (expression == null)
+                    expression = FromRoslyn.Expression(syntax.Expression, this);
+
+                return expression;
+            }
+            set => SetNotNull(ref expression, value);
+        }
+
+        protected override PatternSyntax GetWrapped(ref bool? changed)
+        {
+            GetAndResetChanged(ref changed);
+
+            bool? thisChanged = false;
+
+            var newExpression = expression?.GetWrapped(ref thisChanged) ?? syntax.Expression;
+
+            if (syntax == null || thisChanged == true)
+            {
+                syntax = RoslynSyntaxFactory.ConstantPattern(newExpression);
+
+                SetChanged(ref changed);
+            }
+
+            return syntax;
+        }
+
+        private protected override void SetSyntaxImpl(Roslyn::SyntaxNode newSyntax)
+        {
+            syntax = (ConstantPatternSyntax)newSyntax;
+            Set(ref expression, null);
+        }
+
+        private protected override SyntaxNode CloneImpl() => new ConstantPattern(Expression);
+
+        public override void ReplaceExpressions<T>(Func<T, bool> filter, Func<T, Expression> projection) =>
+            Expression = Expression.ReplaceExpressions(Expression, filter, projection);
+    }
+}
---
+++ src/Syntax/Expressions/Patterns/Pattern.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CSharpE.Syntax.Internals;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace CSharpE.Syntax
+{
+    public abstract class Pattern : SyntaxNode, ISyntaxWrapper<PatternSyntax>
+    {
+        private protected Pattern() { }
+        private protected Pattern(PatternSyntax syntax) : base(syntax) { }
+
+        PatternSyntax ISyntaxWrapper<PatternSyntax>.GetWrapped(ref bool? changed) =>
+            GetWrapped(ref changed);
+
+        protected abstract PatternSyntax GetWrapped(ref bool? changed);
+
+        public abstract void ReplaceExpressions<T>(Func<T, bool> filter, Func<T, Expression> projection)
+            where T : Expression;
+    }
+}
